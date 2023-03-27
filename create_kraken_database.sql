0bcb3727ab62cb14c8c76f5f0f78ba40d8e881cf
John <jdtnichols@gmail.com>
2023-03-26 21:56:40 -0700

    Clarified data types. Removed superfluous columns

--- create_kraken_database.sql
+++ create_kraken_database.sql
@@ -5,15 +5,15 @@ USE kraken;
 CREATE TABLE ethusd_raw (
 	trade_id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     trade_timestamp INT,
-    price FLOAT,
-   volume FLOAT
+    price DOUBLE,
+   volume DOUBLE
 );
 
 CREATE TABLE xbtusd_raw (
 	trade_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	trade_timestamp INT,
-	price FLOAT,
-	volume FLOAT
+	price DOUBLE,
+	volume DOUBLE
 );
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ethusd_id.csv' INTO TABLE ethusd_raw
@@ -24,196 +24,302 @@ LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/xbtusd_id.csv' I
 	COLUMNS TERMINATED BY ','
     LINES TERMINATED BY '\r\n';
     
-CREATE TABLE ethusd_all_trades AS (
+CREATE TABLE ethusd_all_trades (
+	trade_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	trade_timestamp INT,
+	datetime_s CHAR (19),
+	price DOUBLE,
+	volume DOUBLE
+);
+    
+INSERT INTO ethusd_all_trades (
 	SELECT
 	trade_id,
     trade_timestamp,
-    FROM_UNIXTIME(trade_timestamp, '%Y-%m-%d %H:%i:%s') AS datetime_s,
+    FROM_UNIXTIME(trade_timestamp, '%Y-%m-%d %H:%i:%s'),
 	price,
 	volume
 	FROM ethusd_raw
 );
 
-CREATE TABLE xbtusd_all_trades AS (
+CREATE TABLE xbtusd_all_trades (
+	trade_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	trade_timestamp INT,
+	datetime_s CHAR (19),
+	price DOUBLE,
+	volume DOUBLE
+);
+
+INSERT INTO xbtusd_all_trades (
 	SELECT
 	trade_id,
     trade_timestamp,
-    FROM_UNIXTIME(trade_timestamp, '%Y-%m-%d %H:%i:%s') AS datetime_s,
+    FROM_UNIXTIME(trade_timestamp, '%Y-%m-%d %H:%i:%s'),
 	price,
 	volume
 	FROM xbtusd_raw
 );
 
+CREATE TABLE ethusd_day (
+	date CHAR (10),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
 SET SESSION group_concat_max_len = 40000000;
 
-CREATE TABLE ethusd_day AS (
+INSERT INTO ethusd_day (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y-%m-%d') AS date,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y-%m-%d'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM ethusd_all_trades
-	GROUP BY date
-	ORDER BY date ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y-%m-%d')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y-%m-%d') ASC
 );
 
-CREATE TABLE xbtusd_day AS (
+CREATE TABLE xbtusd_day (
+	date CHAR (10),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
+INSERT INTO xbtusd_day (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y-%m-%d') AS date,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y-%m-%d'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM xbtusd_all_trades
-	GROUP BY date
-	ORDER BY date ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y-%m-%d')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y-%m-%d') ASC
 );
 
-CREATE TABLE ethusd_minute AS (
+CREATE TABLE ethusd_minute (
+	datetime_min CHAR (16),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
+INSERT INTO ethusd_minute (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y-%m-%d %H:%i') AS datetime_min,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y-%m-%d %H:%i'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM ethusd_all_trades
-	GROUP BY datetime_min
-	ORDER BY datetime_min ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y-%m-%d %H:%i')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y-%m-%d %H:%i') ASC
 );
 
-CREATE TABLE xbtusd_minute AS (
+CREATE TABLE xbtusd_minute (
+	datetime_min CHAR (16),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
+INSERT INTO xbtusd_minute(
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y-%m-%d %H:%i') AS datetime_min,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
+	DATE_FORMAT(datetime_s, '%Y-%m-%d %H:%i'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
 	MAX(price) AS price_high,
 	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM xbtusd_all_trades
-	GROUP BY datetime_min
-	ORDER BY datetime_min ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y-%m-%d %H:%i')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y-%m-%d %H:%i') ASC
 );
 
-CREATE TABLE ethusd_hour AS (
+CREATE TABLE ethusd_hour (
+	datetime_hour CHAR (13),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
+INSERT INTO ethusd_hour (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y-%m-%d %H') AS datetime_hour,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y-%m-%d %H'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM ethusd_all_trades
-	GROUP BY datetime_hour
-	ORDER BY datetime_hour ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y-%m-%d %H')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y-%m-%d %H') ASC
 );
 
-CREATE TABLE xbtusd_hour AS (
+CREATE TABLE xbtusd_hour (
+	datetime_hour CHAR (13),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
+INSERT INTO xbtusd_hour (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y-%m-%d %H') AS datetime_hour,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y-%m-%d %H'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM xbtusd_all_trades
-	GROUP BY datetime_hour
-	ORDER BY datetime_hour ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y-%m-%d %H')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y-%m-%d %H') ASC
 );
 
-CREATE TABLE ethusd_month AS (
+CREATE TABLE ethusd_month (
+	month CHAR (7),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
+INSERT INTO ethusd_month (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y-%m') AS month,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y-%m'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM ethusd_all_trades
-	GROUP BY month
-	ORDER BY month ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y-%m')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y-%m') ASC
 );
 
-CREATE TABLE xbtusd_month AS (
+CREATE TABLE xbtusd_month (
+	month CHAR (7),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
+ INSERT INTO xbtusd_month (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y-%m') AS month,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y-%m'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM xbtusd_all_trades
-	GROUP BY month
-	ORDER BY month ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y-%m')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y-%m') ASC
+);
+
+CREATE TABLE ethusd_year (
+	year CHAR (4),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
 );
 
 SET SESSION group_concat_max_len = 400000000;
 
-CREATE TABLE ethusd_year AS (
+INSERT INTO ethusd_year (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y') AS year,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price),
+	COUNT(*),
+	SUM(price*volume)
 	FROM ethusd_all_trades
-	GROUP BY year
-	ORDER BY year ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y') ASC
 );
 
-CREATE TABLE xbtusd_year AS (
+CREATE TABLE xbtusd_year (
+	year CHAR (4),
+	price_open DOUBLE,
+    price_high DOUBLE,
+    price_low DOUBLE,
+    price_close DOUBLE,
+    price_avg DOUBLE,
+    num_trades INT,
+	volume_in_dollars DOUBLE
+);
+
+INSERT INTO xbtusd_year (
 	SELECT
-	DATE_FORMAT(datetime_s, '%Y') AS year,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ) AS price_open,
-	MAX(price) AS price_high,
-	MIN(price) AS price_low,
-	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ) AS price_close,
-	COUNT(*) AS num_trades,
-	SUM(price*volume) AS volume_in_dollars,
-	(SUM(price*volume))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_volume,
-	MAX(price) - MIN(price) AS range_hilow,
-	(MAX(price) - MIN(price))/(SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 )) AS price_normalized_range
+	DATE_FORMAT(datetime_s, '%Y'),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s), ',', 1 ),
+	MAX(price),
+	MIN(price),
+	SUBSTRING_INDEX( GROUP_CONCAT(CAST(price AS CHAR) ORDER BY datetime_s DESC), ',', 1 ),
+    AVG(price)g,
+	COUNT(*),
+	SUM(price*volume)
 	FROM xbtusd_all_trades
-	GROUP BY year
-	ORDER BY year ASC
+	GROUP BY DATE_FORMAT(datetime_s, '%Y')
+	ORDER BY DATE_FORMAT(datetime_s, '%Y') ASC
 );
--- join_and_export.sql
+++ join_and_export.sql
@@ -1,6 +1,6 @@
 USE kraken;
 
-SELECT 'date', 'eth_price_open', 'eth_price_high', 'eth_price_low', 'eth_price_close', 'eth_num_trades', 'eth_volume_in_dollars', 'eth_range_hilow', 'xbt_price_open', 'xbt_price_high', 'xbt_price_low', 'xbt_price_close', 'xbt_num_trades', 'xbt_volume_in_dollars', 'xbt_range_hilow'
+SELECT 'date', 'eth_price_open', 'eth_price_high', 'eth_price_low', 'eth_price_close', 'eth_price_avg', 'eth_num_trades', 'eth_volume_in_dollars', 'xbt_price_open', 'xbt_price_high', 'xbt_price_low', 'xbt_price_close', 'xbt_price_avg', 'xbt_num_trades', 'xbt_volume_in_dollars'
 UNION
 SELECT
 ethusd_day.date,
@@ -8,16 +8,16 @@ ethusd_day.price_open,
 ethusd_day.price_high,
 ethusd_day.price_low,
 ethusd_day.price_close,
+ethusd_day.price_avg,
 ethusd_day.num_trades,
 ethusd_day.volume_in_dollars,
-ethusd_day.range_hilow,
 xbtusd_day.price_open,
 xbtusd_day.price_high,
 xbtusd_day.price_low,
 xbtusd_day.price_close,
+xbtusd_day.price_avg,
 xbtusd_day.num_trades,
-xbtusd_day.volume_in_dollars,
-xbtusd_day.range_hilow
+xbtusd_day.volume_in_dollars
 FROM ethusd_day
 INNER JOIN xbtusd_day ON ethusd_day.date = xbtusd_day.date
 INTO OUTFILE '[OUTFILE LOCATION]/xbtusd_ethusd_day_innerjoin.csv'
